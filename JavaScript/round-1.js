e6ade80dd6e0ec887ccec6849c6789abac2bd92e
IzzyCraze365 <johnaisabella3@gmail.com>
2023-04-14 16:58:03 -0400

    I can't spell 'catergory' correctly and it was the problem with pulling in from an array CATEGORY CATEGORY CATEGORY...

--- JavaScript/round-1.js
+++ JavaScript/round-1.js
@@ -2,8 +2,8 @@
 // Jeopardy Board Part 2 - Adding functionality
 // John Isabella III
 
-import placeholderQuestions from "../scripts/placeholder-questions";
-//console.log("Here are the Questions",placeholderQuestions);//! TEST
+import placeholderQuestions from "../scripts/placeholder-questions.js";
+//console.log("Here are the Questions");//! TEST
 
 //List of Variables
 var modal = document.querySelector("#modal");
@@ -12,30 +12,56 @@ console.log("User Input =", playerAnswer);
 let questionButton = document.querySelectorAll(".points"); //Going Fishing
 let guessButton = document.getElementById("guess-btn"); //Going Fishing
 let passButton = document.getElementById("pass-btn"); //Going Fishing
-let catergoryList = ["Nature", "Animals", "Computers","Mythology","History","General","Final"] // TODO Empty Array
+let categoryList = ["Nature", "Animals", "Computers","Mythology","History","General","Final"] // TODO Empty Array
 let Player1Name = "John";
 let Player1Score = 0;
 let Player2Name = "Rob";
 let Player2Score = 0;
 
-// Select the Boards Catergories
-let catergoryName1 = document.querySelector("#catergory1");
-console.log("Catergory1",catergoryName1)
-console.log("Catergory1 =", catergory1);//! TEST
-let catergoryName2 = document.querySelector("#catergory2");
-let catergoryName3 = document.querySelector("#catergory3");
-let catergoryName4 = document.querySelector("#catergory4");
-let catergoryName5 = document.querySelector("#catergory5");
-let catergoryName6 = document.querySelector("#catergory6");
+// Select the Boards Catergories (from HTML)
+let categoryName1 = document.querySelector("#category1");
+let categoryName2 = document.querySelector("#category2");
+let categoryName3 = document.querySelector("#category3");
+let categoryName4 = document.querySelector("#category4");
+let categoryName5 = document.querySelector("#category5");
+let categoryName6 = document.querySelector("#category6");
 let popupQuestion = document.querySelector("#questionInsideModal");
 
+// Assign values to the Catergories
+let selection1 = placeholderQuestions
+.filter((cat) => cat.category === categoryList[0])
+.slice(5);
+ console.log("Length of selection 1",selection1[0],selection1.length); //! Test
+/*let selection2 = placeholderQuestions
+.filter((cat) => cat.category === categoryList[1])
+.slice(5);
+console.log("Length of selection 1",selection1.length); //! Test
+let selection3 = placeholderQuestions
+.filter((cat) => cat.category === categoryList[2])
+.slice(5);
+console.log("Length of selection 1",selection1.length); //! Test
+let selection4 = placeholderQuestions
+.filter((cat) => cat.category === categoryList[3])
+.slice(5);
+console.log("Length of selection 1",selection1.length); //! Test
+let selection5 = placeholderQuestions
+.filter((cat) => cat.category === categoryList[4])
+.slice(5);
+console.log("Length of selection 1",selection1.length); //! Test
+let selection6 = placeholderQuestions
+.filter((cat) => cat.category === categoryList[5])
+.slice(5);
+console.log("Length of selection 1",selection1.length); //! Test */
+
+
 // Populate the Board's Catergories
-catergoryName1.innerText = titleCase(catergoryList[0]);
-catergoryName2.innerText = titleCase(catergoryList[1]);
-catergoryName3.innerText = titleCase(catergoryList[2]);
-catergoryName4.innerText = titleCase(catergoryList[3]);
-catergoryName5.innerText = titleCase(catergoryList[4]);
-catergoryName6.innerText = titleCase(catergoryList[5]);
+categoryName1.innerText = selection1[1].category;
+console.log("category1 Name",categoryName1.category)//! TEST
+categoryName2.innerText = categoryList[1];
+categoryName3.innerText = categoryList[2];
+categoryName4.innerText = categoryList[3];
+categoryName5.innerText = categoryList[4];
+categoryName6.innerText = categoryList[5];
 
 
 
