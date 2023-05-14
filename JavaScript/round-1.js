312098ef3a0e2e8c6b42306c3b08006523061a88
IzzyCraze365 <johnaisabella3@gmail.com>
2023-05-14 19:22:10 -0400

    Added TODO notes

--- JavaScript/round-1.js
+++ JavaScript/round-1.js
@@ -25,6 +25,9 @@ let categoryList = [
   "General",
   "Final",
 ]; // TODO Empty Array
+// TODO Create a function that maps the API for 'different' category names and adds them to the "allCategoryList" Array
+// TODO Create another function that randomly selects 6 categories from the "allCategoryList" Array and adds them to the "categoryList" Array
+// TODO Create a fucntion that randomly selects the 5 questions within that category
 
 class Player {
   constructor(name, score, wager) {
